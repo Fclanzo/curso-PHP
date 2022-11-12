b5124b2251704d0ac2a4ad8c37c040d284015af1
Francesco C. Lanzoni <117127113+Fclanzo@users.noreply.github.com>
2022-11-11 22:17:49 -0300

    entendimento das operações e valores dentro do php

--- index.php
+++ index.php
@@ -1,7 +1,33 @@
 <?php
 echo 'Olá mundo<br>';
 echo 'Francesco C. Lanzoni';
-/*
+/* comentário de várias linhas
 http://localhost/curso-php/index.php
 */
+
+// comentário de uma linhas
+print('<h1>escrevendo</h1>');
+
+print_r('<h2>Escrevendo de novo</h2>');
+
+$nome = 'Francesco';
+$idade = 33;
+$altura = 2.00;
+$fumante = true;
+
+echo '<br>';
+echo $nome . '<br>';
+echo $idade . '<br>';
+echo $altura . '<br>';
+echo $fumante . '<br>';
+echo 'Olá ' . $nome . ' você tem ' . $idade . ' anos';
+
+echo '<hr>';
+echo gettype($nome) . '<br>';
+echo gettype($idade) . '<br>';
+echo gettype($altura) . '<br>';
+echo gettype($fumante) . '<br>';
+$altura = '1.75';
+echo gettype($altura) . '<br>';
+
 ?>
